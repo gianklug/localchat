9972ed785c7da54f0a944131aaa5969fcc02cc4b
Gian Klug <gian.klug@ict-scouts.ch>
2022-07-06 21:49:45 +0200

    Improve UI, Add Range setting for distance, first working prototype

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,10 @@
-<Header></Header>
-<Chat></Chat>
-<style>
-
-</style>
 <script lang="ts">
-  import Header from './Header.svelte';
-  import Chat from './Chat.svelte';
-
-
-
+	import Header from './Header.svelte';
+	import Chat from './Chat.svelte';
 </script>
+
+<Header />
+<Chat />
+
+<style>
+</style>
--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -1,22 +1,88 @@
-<Header></Header>
-<form on:submit|preventDefault>
-  <input bind:value={user} type="text" name="" id="">
-  <button on:click={updateUsername} type="submit">Update Username</button>
-</form>
-<style>
-
-</style>
 <script lang="ts">
-  import Header from './Header.svelte';
-  import { username, range } from '../stores.js';
+	import Header from './Header.svelte';
+	import { username, range } from '../stores.js';
+	import { SvelteToast, toast } from '@zerodevx/svelte-toast';
 
-  let user = '';
-  username.subscribe(value => user = value)
+	let user = '';
+	let searchrange = '';
+	username.subscribe((value) => (user = value));
+	range.subscribe((value) => (searchrange = value));
 
-  function updateUsername() {
-    console.log("updating username")
-    console.log(user);
-      username.set(user);
-  }
+	function updateUsername() {
+		username.set(user);
+		toast.push('Username successfully changed', {
+			theme: {
+				'--toastBackground': '#48BB78',
+				'--toastBarBackground': '#2F855A'
+			}
+		});
+	}
 
+	function updateRange() {
+		range.set(searchrange);
+		toast.push('Range successfully changed', {
+			theme: {
+				'--toastBackground': '#48BB78',
+				'--toastBarBackground': '#2F855A'
+			}
+		});
+	}
 </script>
+
+<Header />
+<SvelteToast />
+
+<div class="settings-form">
+	<form on:submit|preventDefault>
+		<label for="username-input">Username</label>
+		<input bind:value={user} type="text" class="text-input" id="username-input" />
+		<button on:click={updateUsername} type="submit" class="button">Update</button>
+	</form>
+	<form on:submit|preventDefault>
+		<label for="range-input">Search range ({searchrange} km)</label>
+		<input
+			bind:value={searchrange}
+			type="range"
+			class="text-input"
+			id="range-input"
+			min="10"
+			max="150"
+		/>
+		<button on:click={updateRange} type="submit" class="button">Update</button>
+	</form>
+</div>
+<a href="/" class="button">Back</a>
+<style>
+	:root {
+		--toastContainerTop: 5em;
+		--toastContainerLeft: auto;
+	}
+
+	.settings-form {
+		margin-top: 2em;
+		padding: 5px;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	.text-input {
+		padding: 5px;
+		border-radius: 10px;
+	}
+
+	#range-input {
+		align-self: bottom;
+	}
+
+	.button {
+		padding: 5px;
+		border-radius: 10px;
+	}
+
+	.button:hover {
+		background-color: lightgray;
+	}
+
+	.button:active {
+		background-color: gray;
+	}
+</style>
